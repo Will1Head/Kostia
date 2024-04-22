134709cab5db8806e8e5ccea7ba8c27663310a1e
Will1Head <167812240+Will1Head@users.noreply.github.com>
2024-04-22 23:23:08 +0300

    Площа квадрата

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private object area;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Отримуємо довжину сторони квадрата з текстового поля
+                double sideLength = double.Parse(textBox1.Text);
+
+                // Обчислюємо площу квадрата
+                area = sideLength * sideLength;
+
+                // Виводимо результат у текстове поле для виводу
+                textBox1.Text = "Площа квадрата: " + area.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Будь ласка, введіть числове значення для довжини сторони.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
